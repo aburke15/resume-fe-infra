e103526a38388d71ae69db3b8a085329763f96ed
Andre Burke <good_credits.08@icloud.com>
2025-08-10 21:37:01 -0600

    got stuff in a working state, updated index html

--- src/cloud-resume-infra-stack.ts
+++ src/cloud-resume-infra-stack.ts
@@ -5,12 +5,12 @@ import { StorageBlob } from "@cdktf/provider-azurerm/lib/storage-blob";
 import { TerraformStack } from "cdktf";
 import { Construct } from "constructs";
 import { Paths } from "./constants";
+import { StorageAccountStaticWebsiteA } from "@cdktf/provider-azurerm/lib/storage-account-static-website";
 import { CdnProfile } from "@cdktf/provider-azurerm/lib/cdn-profile";
 import { CdnEndpoint } from "@cdktf/provider-azurerm/lib/cdn-endpoint";
-import { DnsCnameRecord } from "@cdktf/provider-azurerm/lib/dns-cname-record";
 import { DataAzurermDnsZone } from "@cdktf/provider-azurerm/lib/data-azurerm-dns-zone";
+import { DnsCnameRecord } from "@cdktf/provider-azurerm/lib/dns-cname-record";
 import { CdnEndpointCustomDomain } from "@cdktf/provider-azurerm/lib/cdn-endpoint-custom-domain";
-import { StorageAccountStaticWebsiteA } from "@cdktf/provider-azurerm/lib/storage-account-static-website";
 import { NullProvider } from "../.gen/providers/null/provider";
 import { Resource as NullResource } from "../.gen/providers/null/resource";
 import * as fs from "fs";
@@ -25,13 +25,19 @@ export class CloudResumeInfraStack extends TerraformStack {
       subscriptionId: process.env.ARM_SUBSCRIPTION_ID,
     });
 
+    // #region existing resources
+
     const resourceGroup = new DataAzurermResourceGroup(this, "existing-rg", {
       name: "aburke-tech-rg",
       provider,
     });
 
-    const storageAccount = new StorageAccount(this, "cloud-resume-stroage-acct", {
-      name: "cloudresumestorage143278",
+    //#endregion
+
+    // #region storage account, static website, storage blobs
+
+    const storageAccount = new StorageAccount(this, "cloud-resume-storage-acct", {
+      name: "cloudresstoracct143278",
       resourceGroupName: resourceGroup.name,
       location: resourceGroup.location,
       accountTier: "Standard",
@@ -39,17 +45,64 @@ export class CloudResumeInfraStack extends TerraformStack {
       accountKind: "StorageV2",
     });
 
-    new StorageAccountStaticWebsiteA(this, "cloud-resume-static-website", {
+    const staticWebsite = new StorageAccountStaticWebsiteA(this, "cloud-resume-static-website", {
       storageAccountId: storageAccount.id,
       indexDocument: "index.html",
       error404Document: "error.html",
     });
 
-    const cdnProfile = new CdnProfile(this, "cloud-resume-cdn-profile", {
-      name: "cloud-resume-cdn-profile",
+    const containerName: string = "$web";
+
+    new StorageBlob(this, "index-html-blob", {
+      name: "index.html",
+      storageAccountName: storageAccount.name,
+      storageContainerName: containerName,
+      type: "Block",
+      contentType: "text/html",
+      source: Paths.indexHtmlPath,
+      dependsOn: [staticWebsite],
+    });
+
+    new StorageBlob(this, "error-html-blob", {
+      name: "error.html",
+      storageAccountName: storageAccount.name,
+      storageContainerName: containerName,
+      type: "Block",
+      contentType: "text/html",
+      source: Paths.errorHtmlPath,
+      dependsOn: [staticWebsite],
+    });
+
+    new StorageBlob(this, "styles-css-blob", {
+      name: "styles.css",
+      storageAccountName: storageAccount.name,
+      storageContainerName: containerName,
+      type: "Block",
+      contentType: "text/css",
+      source: Paths.stylesCssPath,
+      dependsOn: [staticWebsite],
+    });
+
+    new StorageBlob(this, "resume-js-blob", {
+      name: "resume.js",
+      storageAccountName: storageAccount.name,
+      storageContainerName: containerName,
+      type: "Block",
+      contentType: "application/javascript",
+      source: Paths.resumeJsPath,
+      dependsOn: [staticWebsite],
+    });
+
+    //#endregion
+
+    // #region cdn profile, endpoint, custom domain, cname
+
+    const cdnProfile = new CdnProfile(this, "cloud-resume-cdn-profile-v2", {
+      name: "cloud-resume-cdn-profile-v2",
       resourceGroupName: resourceGroup.name,
       location: "global",
       sku: "Standard_Microsoft",
+      dependsOn: [staticWebsite],
     });
 
     const cdnEnpoint = new CdnEndpoint(this, "cloud-resume-cdn-endpoint", {
@@ -67,44 +120,7 @@ export class CloudResumeInfraStack extends TerraformStack {
           httpsPort: 443,
         },
       ],
-    });
-
-    const containerName: string = "$web";
-
-    new StorageBlob(this, "index-html-blob", {
-      name: "index.html",
-      storageAccountName: storageAccount.name,
-      storageContainerName: containerName,
-      type: "Block",
-      contentType: "text/html",
-      source: Paths.indexHtmlPath,
-    });
-
-    new StorageBlob(this, "error-html-blob", {
-      name: "error.html",
-      storageAccountName: storageAccount.name,
-      storageContainerName: containerName,
-      type: "Block",
-      contentType: "text/html",
-      source: Paths.errorHtmlPath,
-    });
-
-    new StorageBlob(this, "styles-css-blob", {
-      name: "styles.css",
-      storageAccountName: storageAccount.name,
-      storageContainerName: containerName,
-      type: "Block",
-      contentType: "text/css",
-      source: Paths.stylesCssPath,
-    });
-
-    new StorageBlob(this, "resume-js-blob", {
-      name: "resume.js",
-      storageAccountName: storageAccount.name,
-      storageContainerName: containerName,
-      type: "Block",
-      contentType: "application/javascript",
-      source: Paths.resumeJsPath,
+      dependsOn: [staticWebsite],
     });
 
     const dnsZone = new DataAzurermDnsZone(this, "existing-dns-zone", {
@@ -113,24 +129,30 @@ export class CloudResumeInfraStack extends TerraformStack {
       provider,
     });
 
-    const cname = new DnsCnameRecord(this, "cloud-resume-cname-record", {
+    const cnameRecord = new DnsCnameRecord(this, "cloud-resume-cname-record", {
       name: "res",
       zoneName: dnsZone.name,
       resourceGroupName: resourceGroup.name,
       ttl: 3600,
       targetResourceId: cdnEnpoint.id,
+      dependsOn: [staticWebsite],
     });
 
-    const cdnCustomDomain = new CdnEndpointCustomDomain(this, "cloud-resume-cdn-custom-domain", {
+    const customDomain = new CdnEndpointCustomDomain(this, "cloud-resume-cdn-custom-domain", {
       name: "res-aburke-tech",
       cdnEndpointId: cdnEnpoint.id,
-      hostName: `${cname.name}.${dnsZone.name}`,
+      hostName: `${cnameRecord.name}.${dnsZone.name}`,
       cdnManagedHttps: {
         certificateType: "Dedicated",
         tlsVersion: "TLS12",
         protocolType: "ServerNameIndication",
       },
+      dependsOn: [staticWebsite],
     });
+
+    //#endregion
+
+    // #region null provider, null resource
 
     const hashFile = (p: string) => crypto.createHash("sha256").update(fs.readFileSync(p)).digest("hex");
 
@@ -156,7 +178,9 @@ export class CloudResumeInfraStack extends TerraformStack {
           ].join(" "),
         },
       ],
-      dependsOn: [cdnCustomDomain],
+      dependsOn: [staticWebsite, customDomain],
     });
+
+    //#endregion
   }
 }
